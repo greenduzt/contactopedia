90bfa92982368ca7147d67c19777007e45fd6ea5
chamara <chamara_sachintha@yahoo.com>
2025-02-05 17:01:10 +1100

    Random contact from AXIOS call completed

--- src/Components/ContactPages/AddRandomContact.jsx
+++ src/Components/ContactPages/AddRandomContact.jsx
@@ -1,7 +1,24 @@
-const AddRandomContact = () => {
+import { getRandomUser } from "../../Utilities/api";
+
+const GetRandomContact = async (props) => {
+  const responseFromAPI = await getRandomUser();
+  console.log(responseFromAPI);
+
+  return props.handleAddRandomContact({
+    name:
+      responseFromAPI.data.first_name + " " + responseFromAPI.data.last_name,
+    email: responseFromAPI.data.email,
+    phone: responseFromAPI.data.phone_number,
+  });
+};
+
+const AddRandomContact = (props) => {
   return (
     <div>
-      <button className="btn btn-danger form-control">
+      <button
+        className="btn btn-success form-control"
+        onClick={() => GetRandomContact(props)}
+      >
         Add Random Contact
       </button>
     </div>
